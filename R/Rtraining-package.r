0f73664c10b12fbbe7e8f934c18878b61328d785
Steph Locke <stephanie.g.locke@gmail.com>
2015-04-13 21:08:47 +0100

    html push attempt

--- R/Rtraining-package.r
+++
@@ -1,7 +0,0 @@
-#' Rtraining.
-#'
-#' @name Rtraining
-#' @docType package
-#' @import data.table httr devtools jsonlite Hmisc magrittr dplyr DiagrammeR shiny knitr foreach
-#' @import memisc optiRum
-NULL
